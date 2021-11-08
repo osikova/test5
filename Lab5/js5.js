0aee35647862496d9080e0ec9aaba40ce961d550
osikova <91030673+osikova@users.noreply.github.com>
2021-11-08 06:42:00 +0300

    Add files via upload

---
+++ Lab5/js5.js
@@ -0,0 +1,17 @@
+function click1(){
+    let re = /^\d+$/;
+    let f1 = document.getElementById("field1");
+    let f2 = document.getElementById("field2");
+    let r = document.getElementById("result");
+    if(re.test(f1.value) && re.test(f2.value)){
+      r.innerHTML = parseInt(f1.value) * parseInt(f2.value);
+    } else{
+      r.innerHTML = "Введите корректные данные";
+    }
+    return false;
+  }
+
+  window.addEventListener('DOMContentLoaded', function (event) {
+    console.log("DOM fully loaded and parsed");
+    document.getElementById("button1").addEventListener("click", click1);
+  });
